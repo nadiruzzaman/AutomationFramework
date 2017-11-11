cc4ca3ab8e9d6834befcc06befe9c3bb414b5866
nadiruzzaman <nadiruzzamanqa@gmail.com>
2017-11-10 22:53:40 -0500

    Add firefox browser with else if statement

--- src/main/java/com/framework/automation/framework/testBase/TestBase.java
+++ src/main/java/com/framework/automation/framework/testBase/TestBase.java
@@ -8,46 +8,47 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestBase {
-	public static final Logger log=Logger.getLogger(TestBase.class.getName());
+	public static final Logger log = Logger.getLogger(TestBase.class.getName());
 	public WebDriver driver;
 	private static ChromeDriverService service;
-	String url="http://automationpractice.com/index.php";
-	String browser="chrome";
-	
-	public void init() throws IOException{
-		selectBrowser(browser);
+	String url = "http://automationpractice.com/index.php";
+	String browser = "firefox";
 
+	public void init() throws IOException {
+		selectBrowser(browser);
 		getUrl(url);
-	String Log4jConfigPath="log4j.properties";
-   PropertyConfigurator.configure(Log4jConfigPath);
-	
+		String Log4jConfigPath = "log4j.properties";
+		PropertyConfigurator.configure(Log4jConfigPath);
+
+	}
+
+	public void selectBrowser(String browser) throws IOException {
+		if (browser.equalsIgnoreCase("chrome")) {
+
+			service = new ChromeDriverService.Builder().usingDriverExecutable(new File("./driver/chromedriver"))
+					.usingAnyFreePort().build();
+			service.start();
+			log.info("creating object of  " + browser);
+			driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.firefox.marionette", System.getProperty("user.dir") + "/driver/deckodriver");
+			log.info("creating object of  " + browser);
+			driver=new FirefoxDriver();
+
+		}
+
+	}
+
+	public void getUrl(String url) {
+		log.info("navigating to " + url);
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+	}
 
 }
-	public void selectBrowser(String browser) throws IOException{
-		if (browser.equalsIgnoreCase("chrome")){
-
-service = new ChromeDriverService.Builder()
-		        .usingDriverExecutable(new File("/Users/nadiruzzaman.muhamm/Documents/PageObject/AutomationFramework/driver/chromedriver"))
-		        .usingAnyFreePort()
-		        .build();
-		    service.start();
-		    log.info("creating object of  "+browser);
-		    driver = new RemoteWebDriver(service.getUrl(),
-		    DesiredCapabilities.chrome());
-		
-		
-		}
-	}
-		public void getUrl(String url){
-			log.info("navigating to "+ url);
-			driver.get(url);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			
-		}
-		
-	}
-	
