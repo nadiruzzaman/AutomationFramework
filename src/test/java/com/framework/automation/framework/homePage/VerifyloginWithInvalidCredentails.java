49ff801534aeb5c5b7564bf7e6e225a80cad317c
kagimuddin <uddin.kagim@gmail.com>
2017-11-25 12:57:50 -0500

    xpath and id modification

--- src/test/java/com/framework/automation/framework/homePage/VerifyloginWithInvalidCredentails.java
+++ src/test/java/com/framework/automation/framework/homePage/VerifyloginWithInvalidCredentails.java
@@ -26,14 +26,6 @@ public class VerifyloginWithInvalidCredentails extends TestBase{
 	 homepage.logInToApplication("test@gmail.com", "test123");
     Assert.assertEquals(homepage.getInvalidLogInText(),"Authentication failed.");
     log.info("***********Finish VerifyloginWithInvalidCredentails Test************");
-	}
- @Test
-	public void validEmailforCreateAccount(){
-	 log.info("***********Starting validEmailforCreateAccount Test************");
-	 homepage = new HomePage(driver);
-	 homepage.correctCreateAccount("automation@gmil.com");
-	 Assert.assertEquals(homepage.validCreateAccount(),"AUTHENTICATION");
- log.info("***********Finish validEmailforCreateAccount Test************");
  }
 	@AfterTest 
 	public void end(){
