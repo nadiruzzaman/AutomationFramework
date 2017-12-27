7399e5edfa0d2b8b8a174120488092516617cb10
nadiruzzaman <nadiruzzamanqa@gmail.com>
2017-12-27 08:07:00 -0500

    FormatCode

--- src/test/java/com/framework/automation/framework/homePage/VerifyloginWithValidCredentails.java
+++ src/test/java/com/framework/automation/framework/homePage/VerifyloginWithValidCredentails.java
@@ -11,41 +11,45 @@ import org.testng.annotations.Test;
 import com.framework.automation.framework.applicationAction.HomePage;
 import com.framework.automation.framework.testBase.TestBase;
 
-public class VerifyloginWithValidCredentails extends TestBase{
+public class VerifyloginWithValidCredentails extends TestBase {
 
-	public static final Logger log=Logger.getLogger(VerifyloginWithValidCredentails.class.getName());
+	public static final Logger log = Logger.getLogger(VerifyloginWithValidCredentails.class.getName());
 	HomePage homepage;
-	@BeforeTest
-	public void setup() throws IOException{
-		init();	
-	}
- @Test
-	public void verifyLoginWithValidCredentails(){
-	 log.info("***********Starting verifyLoginWithValidCredentails Test************");
-	 homepage = new HomePage(driver);
-	 homepage.logInToApplication("kagim2005@gmail.com", "uddin");
-    Assert.assertEquals(homepage.getValidLogInText(),"k u");
-    log.info("***********Finish verifyLoginWithValidCredentails Test************");
-	}
- @Test
- 	public void vrifiedHomePageshoppingChartButtonDisplayed(){
- 	 log.info("***********Starting vrifiedHomePageshoppingChartButtonDisplayed Test************");
- 	 homepage = new HomePage(driver);
-  homepage.vrifiedHomePageshoppingchart();
-  log.info("***********Finish vrifiedHomePageshoppingChartButtonDisplayed Test************");
- }
-@Test
-public void validEmailforCreateAccount(){
- log.info("***********Starting validEmailforCreateAccount Test************");
- homepage = new HomePage(driver);
- homepage.correctCreateAccount("automation@gmil.com");
- Assert.assertEquals(homepage.validCreateAccount(),"AUTHENTICATION");
-log.info("***********Finish validEmailforCreateAccount Test************");
-}
 
-	@AfterTest 
-	public void end(){
-	driver.close();
-		
+	@BeforeTest
+	public void setup() throws IOException {
+		init();
+	}
+
+	@Test
+	public void verifyLoginWithValidCredentails() {
+		log.info("***********Starting verifyLoginWithValidCredentails Test************");
+		homepage = new HomePage(driver);
+		homepage.logInToApplication("kagim2005@gmail.com", "uddin");
+		Assert.assertEquals(homepage.getValidLogInText(), "k u");
+		log.info("***********Finish verifyLoginWithValidCredentails Test************");
+	}
+
+	@Test
+	public void vrifiedHomePageshoppingChartButtonDisplayed() {
+		log.info("***********Starting vrifiedHomePageshoppingChartButtonDisplayed Test************");
+		homepage = new HomePage(driver);
+		homepage.vrifiedHomePageshoppingchart();
+		log.info("***********Finish vrifiedHomePageshoppingChartButtonDisplayed Test************");
+	}
+
+	@Test
+	public void validEmailforCreateAccount() {
+		log.info("***********Starting validEmailforCreateAccount Test************");
+		homepage = new HomePage(driver);
+		homepage.correctCreateAccount("automation@gmil.com");
+		Assert.assertEquals(homepage.validCreateAccount(), "AUTHENTICATION");
+		log.info("***********Finish validEmailforCreateAccount Test************");
+	}
+
+	@AfterTest
+	public void end() {
+		driver.close();
+
 	}
 }
